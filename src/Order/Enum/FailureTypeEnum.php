42cfe8daa25a184be40a9bb5a6038ff2bea6fbd3
Daniel Costa <daniel.costa@mobly.com.br>
2015-01-26 12:38:55 -0200

    Adding Failure Reason enum class

---
+++ src/Order/Enum/FailureTypeEnum.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SellerCenter\SDK\Order\Enum;
+
+use DanielCosta\Enum\Enum;
+
+/**
+ * Class FailureTypeEnum
+ *
+ * @package SellerCenter\SDK\Status\Enum
+ * @author  Daniel Costa
+ * @method static DeliveryTypeEnum CANCELED()
+ * @method static DeliveryTypeEnum FAILED()
+ * @method static DeliveryTypeEnum RETURNED()
+ */
+class FailureTypeEnum extends Enum
+{
+    const CANCELED = 'canceled';
+    const FAILED = 'failed';
+    const RETURNED = 'returned';
+}
