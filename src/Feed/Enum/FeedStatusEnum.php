06ceb9dc7fe99e1989eabf165ecacfba977edab6
Daniel Costa <daniel.costa@mobly.com.br>
2015-02-12 18:23:19 -0200

    Adding FeedStatus enum

---
+++ src/Feed/Enum/FeedStatusEnum.php
@@ -0,0 +1,17 @@
+<?php namespace SellerCenter\SDK\Feed\Enum;
+
+/**
+ * Class FeedStatusEnum
+ *
+ * @package SellerCenter\SDK\Status\Enum
+ * @author  Daniel Costa
+ * @method static FeedStatusEnum QUEUED()
+ * @method static FeedStatusEnum PROCESSING()
+ * @method static FeedStatusEnum FINISHED()
+ */
+class FeedStatusEnum
+{
+    const QUEUED = 'Queued';
+    const PROCESSING = 'Processing';
+    const FINISHED = 'Finished';
+}
