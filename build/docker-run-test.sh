2fed5836eedf65363c7cd8c7e4a377c704774553
Daniel Costa <daniel.costa@mobly.com.br>
2015-12-01 18:50:19 -0200

    Removing tty to allow build from bamboo

--- build/docker-run-test.sh
+++ build/docker-run-test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 #docker run --rm -v ../:/sellercenter-sdk -it mobly/sellercenter-sdk sellercenter-sdk/build/docker-test.sh
-docker run --rm -v `pwd`/..:/sellercenter-sdk -it mobly/sellercenter-sdk sellercenter-sdk/build/docker-test.sh
+docker run --rm -v `pwd`/..:/sellercenter-sdk -i mobly/sellercenter-sdk sellercenter-sdk/build/docker-test.sh
 #docker run --rm -v `pwd`/..:/sellercenter-sdk -it mobly/sellercenter-sdk bash
